7b49cb128d1ee00acc701cd367e907d65b57c200
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-13 08:16:09 +0000

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Prints string
+  *
+  * Return: 1 on failure, 0 on success
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+

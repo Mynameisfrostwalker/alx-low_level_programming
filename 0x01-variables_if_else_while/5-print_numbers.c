daa4476e453ebf054ee49fcda59e7e4c84ea690e
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-14 08:17:10 +0000

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* 5-print_numbers */
+
+/**
+ * main - Print numbers
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	putchar('\n');
+	return (0);
+}
+

3a73c511554a95dfd878b7b2c640d5d74a15a87d
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-14 08:21:14 +0000

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* 7-print_tebahpla */
+
+/**
+ * main - Print reverse alphabet
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
+

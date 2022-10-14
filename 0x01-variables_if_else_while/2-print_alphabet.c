5570d22eb6430b79098e5f5844829dd781d23003
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-14 07:38:36 +0000

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* 2-print_alphabet */
+
+/**
+ * main - Print alphabet
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
+

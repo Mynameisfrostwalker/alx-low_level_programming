e8fe5f099d7f31a17e69d336d352d6bbc0ecde5e
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-19 01:11:10 +0000

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* _abs */
+
+/**
+ * _abs - Returns absolute value of input
+ * @n: Input
+ *
+ * Return: Absolute value of n
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : -n);
+}
+

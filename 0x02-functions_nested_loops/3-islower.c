3896892cf89e53113d3049b78a686de965dcc85d
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-19 00:33:24 +0000

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* _islower */
+
+/**
+ * _islower - Checks if input is lowercase
+ *
+ *
+ * Return: 1 if lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+

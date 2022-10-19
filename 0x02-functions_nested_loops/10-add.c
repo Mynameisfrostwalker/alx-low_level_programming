27ad89d63a148ed24b29f494de97ac3999ec42d5
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-19 02:23:56 +0000

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* add */
+
+/**
+ * add - Adds two numbers
+ * @n: Number 1
+ * @m: Number 2
+ *
+ * Return: Sum of two inputs
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
+

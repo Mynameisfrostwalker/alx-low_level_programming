c7681e1ed0781661a85ffe5c9b342607a7d27b56
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-19 01:51:40 +0000

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/* 24-hours */
+
+/**
+ * jack+bauer - Prints every minute of a day
+ *
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		int a = i % 10;
+		int b = i / 10;
+		for(j = 0; j < 60; j++)
+		{
+			int c = j % 10;
+			int d = j / 10;
+			_putchar('0' + b);
+			_putchar('0' + a);
+			_putchar(':');
+			_putchar('0' + d);
+			_putchar('0' + c);
+			_putchar('\n');
+		}
+	}
+}
+

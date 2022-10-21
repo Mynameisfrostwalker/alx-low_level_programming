3a79e59ea03a998d5f42723a3c88305ca22f7c4f
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-21 01:39:33 +0000

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+* _isdigit - Checks if input is a digit
+* @c: Input
+*
+* Return: 1 if input is a digit, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+

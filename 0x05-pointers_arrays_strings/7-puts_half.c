4903aeec4cd847821dea7b43c4525e20567c2957
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-10-24 22:03:18 +0000

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _strlen - Returns length of string
+* @str: String
+*
+* Return: Length of string
+*/
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+* puts_half - Prints half of string to standard output
+* @str: String
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int y = _strlen(str);
+
+	for (i = y % 2 == 0 ? y / 2 : (y - 1) / 2; i < y; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
